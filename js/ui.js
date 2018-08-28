8f4120381dae7c3b0899b1d58cf4c3fff4ec5f15
michelle <nzoudmich@yahoo.fr>
2018-08-28 07:21:37 -0700

    remove message after 3s

--- js/ui.js
+++ js/ui.js
@@ -1,17 +1,48 @@
 class UI {
-   constructor() {
-       this.init();
+    constructor() {
+         this.init();
     }
-   init () {
-   this.printCryptoCurrencies();
-     }
-     //prints the option for the form
-     printCryptoCurrencies() {
+    init() {
+         this.printCryptoCurrencies();
+    }
+    // Prints the <option> for the form
+    printCryptoCurrencies() {
          cryptoAPI.getCryptoCurrenciesList()
-         .then(function(data) {
-             const cryptocurrencies = data.cryptoCurrencies;
-             console.log(cryptocurrencies);
-         })
-     }
-   
+              .then(data => {
+                   const cryptoCurrencies = data.cryptoCurrencies;
+
+                   // Build the <select> from the REST API
+                   const select = document.getElementById('cryptocurrency');
+
+                   cryptoCurrencies.forEach(currency => {
+                        // add the <option>
+                        const option = document.createElement('option');
+                        option.value = currency.id;
+                        option.appendChild(document.createTextNode(currency.name));
+                        select.appendChild(option);
+                   })
+              })
+    }
+
+
+      // Prints a message 2 parameters, message and classes
+
+      printMessage(message, className) {
+        const div = document.createElement('div');
+
+        // add the classes
+        div.className = className;
+        // add the message
+        div.appendChild(document.createTextNode(message));
+
+        const messagesDiv = document.querySelector('.messages');
+
+        messagesDiv.appendChild(div);
+
+        // Remove the message
+        setTimeout(()  => {
+             document.querySelector('.messages div').remove();
+        }, 3000); 
+   }
+
 }
